0788fba463a741f793a359eb142e1ac55d259086
zjradfor <zjradfor@uwaterloo.ca>
2021-04-01 20:37:28 -0400

    Add collectionView

--- Etch.playground/Contents.swift
+++ Etch.playground/Contents.swift
@@ -1,21 +1,78 @@
-//: A UIKit based Playground for presenting user interface
-  
+
 import UIKit
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
+class MainViewController: UIViewController {
+
+    private let layoutGuide: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+        layout.itemSize = CGSize(width: 30, height: 30)
+
+        return layout
+    }()
+
+    private lazy var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: layoutGuide)
+
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
 
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
         self.view = view
+
+        buildUI()
+    }
+
+    private func buildUI() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        view.addSubview(collectionView)
+
+        collectionView.pinEdges(to: view)
     }
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+
+extension MainViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        9
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        9
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+        cell.backgroundColor = .red
+
+        return cell
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+}
+
+// MARK: - Utils
+
+extension UIView {
+    func activateConstraints(_ constraints: [NSLayoutConstraint]) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(constraints)
+    }
+
+    func pinEdges(to view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
+
+let vc = MainViewController()
+PlaygroundPage.current.liveView = vc
