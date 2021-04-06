cd7d8a76864fa35715f77d8cfcfb1ee53730ef1f
zjradfor <zjradfor@uwaterloo.ca>
2021-04-05 20:35:04 -0400

    Create grid and colour cells

--- Etch.playground/Contents.swift
+++ Etch.playground/Contents.swift
@@ -4,48 +4,77 @@ import PlaygroundSupport
 
 class MainViewController: UIViewController {
 
-    private let layoutGuide: UICollectionViewFlowLayout = {
+    private let collectionViewLayout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
-        layout.itemSize = CGSize(width: 30, height: 30)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
 
         return layout
     }()
 
-    private lazy var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: layoutGuide)
+    private lazy var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+
+    private let rows: Int = 32
+    private let columns: Int = 32
+
+    private var gridArray: [[UIColor]] = Array(repeating: Array(repeating: .red, count: 32), count: 32)
 
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
 
         self.view = view
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
         buildUI()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let width = collectionView.frame.width / CGFloat(columns)
+        let height = collectionView.frame.height / CGFloat(rows)
+
+        /// Setting a static size instead of using the delegate.
+        /// The delegate was being called too many times for the number of cells and hurting playground performance.
+        collectionViewLayout.itemSize = CGSize(width: width, height: height)
+        // If you decide to resize later, make a method to update itemSize
+        collectionView.reloadData() //TODO: hide collection view before its reloaded so no flash
     }
 
     private func buildUI() {
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
         collectionView.dataSource = self
         collectionView.delegate = self
+        collectionView.backgroundColor = .clear
 
         view.addSubview(collectionView)
 
-        collectionView.pinEdges(to: view)
+        collectionView.activateConstraints([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
+            collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor)
+        ])
     }
 }
 
 extension MainViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        9
+        rows
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        9
+        columns
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
-        cell.backgroundColor = .red
+
+        cell.backgroundColor = gridArray[indexPath.row][indexPath.section]
 
         return cell
     }
@@ -53,6 +82,8 @@ extension MainViewController: UICollectionViewDataSource {
 
 extension MainViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        gridArray[indexPath.row][indexPath.section] = .blue
+        collectionView.reloadItems(at: [indexPath])
         print(indexPath)
     }
 }
